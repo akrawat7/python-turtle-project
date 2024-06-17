ea1060a7a820717b80587a1ba19a8446bba76f3b
akrawat7 <sangkhowakrawat@gmail.com>
2024-06-17 08:57:42 +0700

    Add files via upload

---
+++ Aon.py
@@ -0,0 +1,17 @@
+import turtle
+
+Aon = turtle.Turtle()
+
+Aon.pencolor("blue")
+
+Aon.pendown
+Aon.forward(100)
+Aon.left(90)
+Aon.forward(100)
+Aon.left(90)
+Aon.forward(100)
+Aon.left(90)
+Aon.forward(100)
+Aon.left(90)
+Aon.forward(100)
+Aon.left(90)
