2d91fa05dfa5045a0f7ea21815bdafd3ed149f41
akrawat7 <sangkhowakrawat@gmail.com>
2024-06-17 09:05:45 +0700

    First Update

--- Aon.py
+++ Aon.py
@@ -3,8 +3,9 @@ import turtle
 Aon = turtle.Turtle()
 
 Aon.pencolor("blue")
-
 Aon.pendown
+Aon.fillcolor("red")
+Aon.begin_fill()
 Aon.forward(100)
 Aon.left(90)
 Aon.forward(100)
@@ -12,6 +13,4 @@ Aon.left(90)
 Aon.forward(100)
 Aon.left(90)
 Aon.forward(100)
-Aon.left(90)
-Aon.forward(100)
-Aon.left(90)
+Aon.end_fill()
